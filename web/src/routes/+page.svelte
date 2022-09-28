139d066faf6d679ea556c452167a31e8a59487de
Nick Lehmann <nick@lehmann.sh>
2022-09-28 10:00:50 +0200

    Revert "Web app"

--- web/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { getAuthorizeUrl } from '../lib/spotify/url.js'
-
-  function redirectToSpotify() {
-    window.open(getAuthorizeUrl(), '_self', 'WIDTH=400,HEIGHT=500')
-  }
-</script>
-
-<button on:click={redirectToSpotify}>Login to Spotify</button>
